0cfb8e6c1596d51afa4d8617c7f4940f9703e04f
pwo0123 <taz.dhillon@gmail.com>
2019-09-10 10:43:44 -0700

    added about page for testing the squad info

--- vTrak/urls.py
+++ vTrak/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='vTrak-home'),
-    path('/about', views.about, name='vTrak-about'),
+    path('about', views.about, name='vTrak-about'),
 ]
