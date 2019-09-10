de5499854f7e24c17b83e1790cda326e0a480afc
pwo0123 <taz.dhillon@gmail.com>
2019-09-10 10:03:16 -0700

    added squad view but not working right.

--- vTrak/views.py
+++ vTrak/views.py
@@ -4,11 +4,19 @@ from .models import Vehicletable, Statustable, Squadtable
 
 
 def home(request):
-    Squadinfo = {
+    squadinfo = {
         'Squadinfo': Squadtable.objects.all()
     }
     vehinfo = {
         'Vehicleinfo': Vehicletable.objects.all()
     }
 
-    return render(request, 'vTrak/home.html', vehinfo, Squadinfo)
+    return render(request, 'vTrak/home.html', vehinfo, squadinfo)
+
+
+def about(request):
+    squadinfo = {
+        'Squadinfo': Squadtable.objects.all()
+    }
+
+    return render(request, 'vTrak/about.html', squadinfo)
